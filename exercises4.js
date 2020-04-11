f4d44c08bbcd47e737e30fe8f2b4cdabe13e16a3
liandasari17 <62257074+liandasari17@users.noreply.github.com>
2020-04-11 17:11:48 +0700

    ya

--- exercises4.js
+++ exercises4.js
@@ -1,28 +1,39 @@
 function cariModus(arr) {
-    var tampung = []
-    for(var i=0; i<arr.length; i++){
-      for(var j=0; j< arr.length; j++){
-        if(arr[j] > arr[j+1]){
-          temp= arr[j+1]
-          arr[j+1] = arr[j]
-          arr[j]= temp
-        }
+  var res = []
+  for(var i=0; i<arr.length; i++){
+    //console.log(arr[i])
+    var check = true
+    for(var j=0; j<res.length; j++){
+      //console.log(res[j])
+      if(res[j][0] === arr[i]){
+        res[j].push(arr[i])
+        check = false
       }
     }
-    //console.log(arr)
-    for(var k=0; k<arr.length; k++){
-      if(arr[k] === arr[k+1]){
-        tampung.push(arr[k])
-      } 
+    if(check === true){
+      res.push([arr[i]])
     }
-    return tampung
+  }
+  //console.log(res)
+  for(var i=0; i<res.length; i++){
+    for(var j=0; j<res.length-1; j++){
+      if(res[i].length > res[j].length){
+        var temp = res[j]
+        res[j] = res[i]
+        res[i] = temp
+      }
     }
-    
-    // TEST CASES
-    
-    console.log(cariModus([10, 4, 5, 2, 4])); // 4
-    console.log(cariModus([5, 10, 10, 6, 5])); // 5
-    console.log(cariModus([10, 3, 1, 2, 5])); // -1
-    console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
-    console.log(cariModus([7, 7, 7, 7, 7])); // -1
-    
+  }
+  //console.log(res)
+  if(res.length === arr.length || res.length === 1){
+    return -1
+  }
+  return res[0][0]
+}
+
+// TEST CASES
+console.log(cariModus([10, 4, 5, 2, 4])); // 4
+console.log(cariModus([5, 10, 10, 6, 5])); // 5
+console.log(cariModus([10, 3, 1, 2, 5])); // -1
+console.log(cariModus([1, 2, 3, 3, 4, 5])); // 3
+console.log(cariModus([7, 7, 7, 7, 7])); // -1
